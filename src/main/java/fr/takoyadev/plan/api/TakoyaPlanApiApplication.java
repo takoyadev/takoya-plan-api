07e671c86acc00b68cd007e95ebe4a79d8c59481
chinjto <contact@cyril-defaye.fr>
2022-03-25 22:47:43 +0100

    init project from spring schematic

---
+++ src/main/java/fr/takoyadev/plan/api/TakoyaPlanApiApplication.java
@@ -0,0 +1,19 @@
+package fr.takoyadev.plan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principale de l'API
+ *
+ * @author chinjto
+ * @version 1.0
+ */
+@SpringBootApplication
+public class TakoyaPlanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakoyaPlanApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/takoyadev/plan/api/TakoyaPlanApiApplicationTests.java
@@ -0,0 +1,19 @@
+package fr.takoyadev.plan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Classe principale des tests de l'API
+ *
+ * @author chinjto
+ * @version 1.0
+ */
+@SpringBootTest
+class TakoyaPlanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
